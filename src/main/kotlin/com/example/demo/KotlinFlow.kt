a69e180c6f85157a9f21db9c4a25848c58fdb1f7
hantsy <hantsy@gmail.com>
2022-11-20 14:42:54 +0800

    clean codes

--- src/main/kotlin/com/example/demo/KotlinFlow.kt
+++ src/main/kotlin/com/example/demo/KotlinFlow.kt
@@ -1,8 +1,10 @@
 package com.example.demo
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 
 suspend fun main(args: Array<String>) {
     flow {
@@ -12,6 +14,8 @@ suspend fun main(args: Array<String>) {
             emit(it)
         }
     }
+        .onEach { println("emitted value: $it") }
+        .filter { it % 3 == 0 }
         .map { it * 2 }
         .collect { println("collected result: $it") }
 }
