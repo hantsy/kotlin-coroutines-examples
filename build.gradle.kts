3613208b2020d28613a6e4bf11f92468f011d288
Hantsy Bai <hantsy@gmail.com>
2024-06-13 12:11:31 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ val mockkVersion = project.properties["mockkVersion"]
 
 dependencies {
 	// import boms
-	implementation(platform("io.projectreactor:reactor-bom:2023.0.7"))
+	implementation(platform("io.projectreactor:reactor-bom:2024.0.0-M3"))
 
 	// build
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
